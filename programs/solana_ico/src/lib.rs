4ca78fe7de481485a7fd6ac41c63f5a77a970c27
jaydippatel83 <jaydippatel83.pj@gmail.com>
2025-12-12 16:47:29 +0530

    add deposit token functions

--- programs/solana_ico/src/lib.rs
+++ programs/solana_ico/src/lib.rs
@@ -21,13 +21,52 @@ pub mod solana_ico {
 
 
     pub fn create_ico(ctx: Context<CreateIco>,ico_amount: u64)->Result<()>{
+         msg!("Creating program to hold ICO tokens");
+         let raw_ammount = ico_amount.checked_mul(TOKEN_DECIMALS).ok_or(ErrorCode::Overflow);
+         let cpi_ctx= CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            token::Transfer {
+                from: ctx.accounts.admin.to_account_info(),
+                to: ctx.accounts.ico_mint.to_account_info(),
+                authority: ctx.accounts.admin.to_account_info(),
+            },
+        );
+        token::transfer(cpi_ctx, raw_ammount)?;
+        msg!("ICO mint created successfully",ico_amount); 
+
+        let data = &mut ctx.accounts.data;
+        data.admin = *ctx.accounts.admin.key;
+        data.token_sold = 0;
+        msg!("ICO created successfully");
         Ok(())
+         
     }
 
 
-    pub fn deposite_ico(){}
+    pub fn deposite_ico(ctx: Context<DepositIco>,amount: u64)->Result<()>{
+        if ctx.accounts.data.admin != *ctx.accounts.admin.key{
+            return Err(error!(ErrorCode::InvalidAdmin));
+        }
 
-    
+        let raw_ammount = ico_amount.checked_mul(TOKEN_DECIMALS).ok_or(ErrorCode::Overflow);
+         let cpi_ctx= CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            token::Transfer {
+                from: ctx.accounts.admin.to_account_info(),
+                to: ctx.accounts.ico_mint.to_account_info(),
+                authority: ctx.accounts.admin.to_account_info(),
+            },
+        ); 
+        token::transfer(cpi_ctx, raw_ammount)?;
+
+        let data = &mut ctx.accounts.data;
+        data.total_tokens += ico_amount;
+        msg!("Deposited ICO tokens successfully",ico_amount); 
+        Ok(())
+
+    }
+
+
     pub fn buy_tokens(){}
 
     #[derive(Accounts)]
